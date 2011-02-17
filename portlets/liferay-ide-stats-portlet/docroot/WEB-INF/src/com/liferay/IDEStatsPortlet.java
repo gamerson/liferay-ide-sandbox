fa79073acd4ac4696f9cf8b5d8ec7756b3216081
gregory.amerson <gregory.amerson@liferay.com>
2011-02-16 19:37:09 -0600

    Signed-off-by: gregory.amerson <gregory.amerson@liferay.com>

---
+++ portlets/liferay-ide-stats-portlet/docroot/WEB-INF/src/com/liferay/IDEStatsPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class IDEStatsPortlet
+ */
+public class IDEStatsPortlet extends MVCPortlet {
+ 
+
+}
